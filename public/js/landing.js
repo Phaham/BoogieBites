15350e162ee07a09102d4663e50a31831309375e
PHAHAM KHAN <72141945+Phaham@users.noreply.github.com>
2024-08-08 14:02:01 +0530

    Update landing.js

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,25 +1,50 @@
-const main_dish = document.querySelector('.main-dish');
-const dish1 = document.querySelector('.dish-1');
-const dish2 = document.querySelector('.dish-2');
-const dish3 = document.querySelector('.dish-3');
-const dish4 = document.querySelector('.dish-4');
-const dish5 = document.querySelector('.dish-5');
+// const main_dish = document.querySelector('.main-dish');
+// const dish1 = document.querySelector('.dish-1');
+// const dish2 = document.querySelector('.dish-2');
+// const dish3 = document.querySelector('.dish-3');
+// const dish4 = document.querySelector('.dish-4');
+// const dish5 = document.querySelector('.dish-5');
 
-dish1.addEventListener('click', () => {
-    main_dish.src = "/img/dishes/image_3.png";
-})
-dish2.addEventListener('click', () => {
-    main_dish.src = "/img/dishes/main_2.png";
-})
-dish3.addEventListener('click', () => {
-    main_dish.src = "/img/dishes/image_6.png";
-})
-dish4.addEventListener('click', () => {
-    main_dish.src = "/img/dishes/image_5.png";
-})
-dish5.addEventListener('click', () => {
-    main_dish.src = "/img/dishes/dish1.png";
-})
+// dish1.addEventListener('click', () => {
+//     main_dish.src = "/img/dishes/image_3.png";
+// })
+// dish2.addEventListener('click', () => {
+//     main_dish.src = "/img/dishes/main_2.png";
+// })
+// dish3.addEventListener('click', () => {
+//     main_dish.src = "/img/dishes/image_6.png";
+// })
+// dish4.addEventListener('click', () => {
+//     main_dish.src = "/img/dishes/image_5.png";
+// })
+// dish5.addEventListener('click', () => {
+//     main_dish.src = "/img/dishes/dish1.png";
+// })
+document.addEventListener('DOMContentLoaded', () => {
+    const main_dish = document.querySelector('.main-dish');
+    const dish1 = document.querySelector('.dish-1');
+    const dish2 = document.querySelector('.dish-2');
+    const dish3 = document.querySelector('.dish-3');
+    const dish4 = document.querySelector('.dish-4');
+    const dish5 = document.querySelector('.dish-5');
+
+    dish1.addEventListener('click', () => {
+        main_dish.src = "/img/dishes/image_3.png";
+    });
+    dish2.addEventListener('click', () => {
+        main_dish.src = "/img/dishes/main_2.png";
+    })
+    dish3.addEventListener('click', () => {
+        main_dish.src = "/img/dishes/image_6.png";
+    })
+    dish4.addEventListener('click', () => {
+        main_dish.src = "/img/dishes/image_5.png";
+    })
+    dish5.addEventListener('click', () => {
+        main_dish.src = "/img/dishes/dish1.png";
+    })
+});
+
 
 
 // search-bar
